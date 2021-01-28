8ef34a7eb68e81e8af94a3471c9175e9b1bd940b
Sahud <khansahud@gmail.com>
2021-01-28 19:39:39 +0530

    new

--- js/custom.js
+++ js/custom.js
@@ -16,53 +16,6 @@
     });
 
 <!--video gallery-->
-$('#video-gallery').lightGallery();
-
- // Tranding carousel
-$('.owl-carousel').owlCarousel({
-    stagePadding: 200,
-    loop:true,
-    margin:10,
-    items:1,
-    lazyLoad: true,
-    nav:true,
-	 navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-  responsive:{
-        0:{
-            items:1,
-            stagePadding: 60
-        },
-        600:{
-            items:1,
-            stagePadding: 100
-        },
-        1000:{
-            items:1,
-            stagePadding: 200
-        },
-        1200:{
-            items:1,
-            stagePadding: 250
-        },
-        1400:{
-            items:1,
-            stagePadding: 300
-        },
-        1600:{
-            items:1,
-            stagePadding: 350
-        },
-        1800:{
-            items:1,
-            stagePadding: 400
-        }
-    }
-})
-
-
 
 <!--sticky left right-->
 $( '.right_side_flex' ).fixedsticky();
